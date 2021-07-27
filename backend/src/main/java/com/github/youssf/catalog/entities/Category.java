694ad1bf650dc9897ab49adc7be877d25438de14
Alisson Youssf <youssfbr@gmail.com>
2021-07-27 18:51:20 -0300

    Auditing data

--- backend/src/main/java/com/github/youssf/catalog/entities/Category.java
+++ backend/src/main/java/com/github/youssf/catalog/entities/Category.java
@@ -1,6 +1,7 @@
 package com.github.youssf.catalog.entities;
 
 import javax.persistence.*;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -12,6 +13,12 @@ public class Category {
     private Long id;
 
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Long getId() {
         return id;
@@ -27,6 +34,24 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    private void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
